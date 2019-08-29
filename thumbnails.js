911cb2f49facd9fc55e0adbb263742200cda739c
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2019-08-29 09:13:48 -0500

    Doing thumbnail part II final

--- thumbnails.js
+++ thumbnails.js
@@ -1,16 +1,19 @@
 const handleThumbnail = document.querySelector(".section_thumbnail");
 const $bigImage = document.querySelector(".bigImage");
+const $bgOveryl = document.getElementById("bgOverly");
 const classPopUp = document.querySelector(".section_popup_off");
-let onPopUp = false;
+let onPopUp = true;
 let popUpClass = "";
 
 handleThumbnail.addEventListener("click", ev => {
   $bigImage.src = ev.target.src;
-  if (!onPopUp) {
+  if (onPopUp) {
     classPopUp.className = "section_popup_on";
+    $bgOveryl.className = "section_popup_bgOverly";
   }
 });
 
 function handleClosedPopUp() {
   classPopUp.className = "section_popup_off";
+  $bgOveryl.className = "";
 }
