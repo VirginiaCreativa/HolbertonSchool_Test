179e73521601010fb0edf2a81a94436413ff3114
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2019-08-29 11:42:05 -0500

    Add new page, final complet

--- behavior.js
+++ behavior.js
@@ -1,1 +1,19 @@
-alert("Hello");
+const handleThumbnail = document.querySelector(".section_thumbnail");
+const $bigImage = document.querySelector(".bigImage");
+const $bgOveryl = document.getElementById("bgOverly");
+const classPopUp = document.querySelector(".section_popup_off");
+let onPopUp = true;
+let popUpClass = "";
+
+handleThumbnail.addEventListener("click", ev => {
+  $bigImage.src = ev.target.src;
+  if (onPopUp) {
+    classPopUp.className = "section_popup_on";
+    $bgOveryl.className = "section_popup_bgOverly";
+  }
+});
+
+function handleClosedPopUp() {
+  classPopUp.className = "section_popup_off";
+  $bgOveryl.className = "";
+}
